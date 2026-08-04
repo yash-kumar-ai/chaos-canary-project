c871becbd615282f8d05f593223e614f46333e19
Yash Kumar <yashkumargravity@gmail.com>
2026-08-05 02:52:09 +0530

    Adding Metrics endpoints for prometheus to collect data from

--- app/v1/main.py
+++ app/v1/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import socket
 import os
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 
 app = FastAPI(
     title="Chaos Canary Deployer",
@@ -14,8 +16,15 @@ APP_NAME =os.getenv("APP_NAME", "Chaos Canary API")
 APP_VERSION = os.getenv("APP_VERSION", "v1")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "Development")
 
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests receieved"
+)
+
 @app.get("/")
 def home():
+    REQUEST_COUNT.inc()
+
     return {
         "application": APP_NAME,
         "version": APP_VERSION,
@@ -34,3 +43,9 @@ def health():
     return {
         "status": "healthy"
     }
+@app.get("/metrics")
+def metrics():
+    return Response(
+        content=generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+    )
