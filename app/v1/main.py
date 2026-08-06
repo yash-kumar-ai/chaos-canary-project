a526b8871f5dd733e64a9cb0f989a324446ff714
Yash Kumar <yashkumargravity@gmail.com>
2026-08-07 00:49:08 +0530

    adding /fail in v1

--- app/v1/main.py
+++ app/v1/main.py
@@ -3,6 +3,7 @@ import socket
 import os
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 from fastapi.responses import Response
+from fastapi import HTTPException, status
 
 app = FastAPI(
     title="Chaos Canary Deployer",
@@ -16,14 +17,26 @@ APP_NAME =os.getenv("APP_NAME", "Chaos Canary API")
 APP_VERSION = os.getenv("APP_VERSION", "v1")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "Development")
 
-REQUEST_COUNT = Counter(
+REQUEST_COUNTER = Counter(
     "http_requests_total",
-    "Total number of HTTP requests receieved"
+    "Total HTTP requests"
 )
+
+SUCCESS_COUNTER = Counter(
+    "http_requests_success_total",
+    "successful HTTP requests"
+)
+
+ERROR_COUNTER = Counter(
+    "http_requests_error_total",
+    "Failed HTTP requests"
+)
+
 
 @app.get("/")
 def home():
-    REQUEST_COUNT.inc()
+    REQUEST_COUNTER.inc()
+    SUCCESS_COUNTER.inc()
 
     return {
         "application": APP_NAME,
@@ -31,6 +44,16 @@ def home():
         "environment": ENVIRONMENT,
         "hostname": HOSTNAME
     }
+@app.get("/fail")
+def fail():
+    REQUEST_COUNTER.inc()
+    ERROR_COUNTER.inc()
+
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Simulated application failure"
+        
+    )
 @app.get("/version")
 def version():
     return {
